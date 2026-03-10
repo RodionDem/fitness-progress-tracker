1f1af40e660b200465552cd3aed28086a02061f0
Rodion <r.demianetss@gmail.com>
2026-03-10 12:18:05 +0200

    add build.sh and configure production settings

--- fitness_progress_tracker/settings/prod.py
+++ fitness_progress_tracker/settings/prod.py
@@ -7,7 +7,9 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
